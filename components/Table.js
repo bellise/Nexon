cf67b2cbfcf1b797c7ef22a7c5f18db06ca6d072
Choco Bae <bellise03@gmail.com>
2022-03-22 13:54:20 +0300

    main

---
+++ components/Table.js
@@ -0,0 +1,41 @@
+/* eslint-disable react/jsx-key */
+// src/Table.js
+import React from "react";
+import { useTable } from "react-table";
+function Table({ columns, data }) {
+  // Using the state and functions returned from useTable to build your UI
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({
+      columns,
+      data,
+    });
+
+  // Rendering the UI for  table
+  return (
+    <table {...getTableProps()} border="0.5" cellSpacing={20} cellPadding={25} style={{ paddingLeft:50 }}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
--- components/Transactions.js
+++ components/Transactions.js
@@ -1,25 +1,103 @@
-import { Table, Tag, Space } from 'antd';
-import transaction from './data'
-const { Column, ColumnGroup } = Table;
+import React from "react";
+import Table from "./Table";
+import { useMemo } from "react";
 
+const getData = () => [
+  {
+    name: "Jane Cooper",
+    email: "jane.cooper@example.com",
+    title: "Regional Paradigm Technician",
+    department: "Optimization",
+    status: "Active",
+    role: "Admin",
+    imgUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+  {
+    name: "Cody Fisher",
+    email: "cody.fisher@example.com",
+    title: "Product Directives Officer",
+    department: "Intranet",
+    status: "Active",
+    role: "Owner",
+    imgUrl:
+      "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+  {
+    name: "Esther Howard",
+    email: "esther.howard@example.com",
+    title: "Forward Response Developer",
+    department: "Directives",
+    status: "Active",
+    role: "Member",
+    imgUrl:
+      "https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+  {
+    name: "Jenny Wilson",
+    email: "jenny.wilson@example.com",
+    title: "Central Security Manager",
+    department: "Program",
+    status: "Active",
+    role: "Member",
+    imgUrl:
+      "https://images.unsplash.com/photo-1498551172505-8ee7ad69f235?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+  {
+    name: "Kristin Watson",
+    email: "kristin.watson@example.com",
+    title: "Lean Implementation Liaison",
+    department: "Mobility",
+    status: "Active",
+    role: "Admin",
+    imgUrl:
+      "https://images.unsplash.com/photo-1532417344469-368f9ae6d187?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+  {
+    name: "Cameron Williamson",
+    email: "cameron.williamson@example.com",
+    title: "Internal Applications Engineer",
+    department: "Security",
+    status: "Active",
+    role: "Member",
+    imgUrl:
+      "https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+];
 
+function App() {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Name",
+        accessor: "name",
+      },
+      {
+        Header: "Title",
+        accessor: "title",
+      },
+      {
+        Header: "Status",
+        accessor: "status",
+      },
+      {
+        Header: "Role",
+        accessor: "role",
+      },
+    ],
+    []
+  );
 
-const Transactions = () => {
-    return(
-      <div>
-       {transaction ?.map((info) => (
-       // eslint-disable-next-line react/jsx-key
-       <Table>
-      <ColumnGroup title="Name">
-      <Column title="First Name" dataIndex="firstName" key="firstName" />
-      <Column title="Last Name" dataIndex="lastName" key="lastName" />
-      <Column title="Age" dataIndex="age" key="age" />
-    <Column title="Address" dataIndex="address" key="address" />
-    </ColumnGroup>
-  </Table>
-    ))}
+  const data = React.useMemo(() => getData(), []);
+
+  return (
+    <>
+      <h1>Hello React!</h1>
+      <div className='padding'>
+        <Table columns={columns} data={data} />
       </div>
-
-    )
+    </>
+  );
 }
-export default Table;
+
+export default App;
